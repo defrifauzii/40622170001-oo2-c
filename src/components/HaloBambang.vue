addcb7df5512cb33c6a8bdc72b61c89feedd137b
defri <defridalopez08@gmail.com>
2024-03-19 14:29:09 +0700

    init repo

---
+++ src/components/HaloBambang.vue
@@ -0,0 +1,33 @@
+<template>
+    <h1>Hi Bambang</h1>
+    <br>
+    <h1>umurnya berapa ? {{ count }}</h1> <!--kurung {} dua kali untuk merender ke html-->
+    <br>
+    <button @click="increment">Count</button> <!--"@" event--> 
+</template>
+<script>
+export default {
+  // Properties returned from data() become reactive state
+  // and will be exposed on `this`.
+  data() {
+    return {
+      count: 0
+    }
+  },
+
+  // Methods are functions that mutate state and trigger updates.
+  // They can be bound as event handlers in templates.
+  methods: {
+    increment() {
+      this.count++
+    }
+  },
+
+  // Lifecycle hooks are called at different stages
+  // of a component's lifecycle.
+  // This function will be called when the component is mounted.
+  mounted() {
+    console.log(`The initial count is ${this.count}.`)
+  }
+}
+</script>
